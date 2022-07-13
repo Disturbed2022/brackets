d3d86a831722ef9fb4485a8a3e32b2a56dfa4a05
Ivan Bortnik <disturbe2011@yandex.ru>
2022-07-13 13:37:22 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = []
+  let i = 0
+  bracketsConfig.map((el) => {
+    arr.push(el.join(""))
+  })
+  do {
+    let newArr = arr[i];
+    if (str.includes(newArr)) {
+      str = str.replace(newArr, '')
+      i = -1
+    }
+    i++
+  } while (i < arr.length)
+  return str.length === 0
 }
